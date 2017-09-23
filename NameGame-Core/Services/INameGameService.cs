fa022a0c21ab4840858a9fedf3b8de9db575c861
xenosavant <Dom@Domenicks-MacBook-Pro.local>
2017-09-23 13:14:37 -0400

    Remove UWP and iOS projects and add INameGameService interface

---
+++ NameGame-Core/Services/INameGameService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using WillowTree.NameGame.Core.Models;
+
+namespace WillowTree.NameGame.Core.Services
+{
+    public interface INameGameService
+    {
+        Task<Profile[]> GetProfiles();
+    }
+}
